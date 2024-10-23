8fbb88e40c2bcec5e97fb2f9002335733d19c358
samuelmora01 <samuelmorafrancozl@gmail.com>
2024-10-23 06:47:08 -0500

    first commit

--- js/punto5.js
+++ js/punto5.js
@@ -1,16 +1,29 @@
+let num1 = parseInt(prompt('Ingrese un numero'))
+let num2 = parseInt(prompt('Ingrese un numero'))
+let num3 = parseInt(prompt('Ingrese un numero'))
 
-let numero1 = parseInt(prompt("digite el primer valor "))
-let numero2 = parseInt(prompt("digite el segundo valor "))
-let numero3 = parseInt(prompt("digite el tercer valor "))
-
-if (numero1 > numero2 && numero1 > numero3 ){
-    console.log(numero1 + "es mayor que: " + numero2 , "y que", + numero3)
-}
-
-else  {
-    if(numero3 > numero1 && numero3 > numero2 ){
-        console.log(numero3 + "es mayor que: " + numero1 , "y que", + numero2 )
+if (num1 > num2 && num1 > num3) {
+    console.log(`El numero mayor es el primero (${num1})`);
+}else{
+    if (num2 > num1 && num2 > num3) {
+        console.log(`El numero mayor es el segundo (${num2})`);
     }else{
-        console.log(numero2 + "es mayor que: " + numero1 , "y que", +numero3)
+        console.log(`El numero mayor es el tercero (${num3})`);
     }
 }
+
+if (num1 == num2) {
+    console.log(`El primer y segundo numero son iguales (${num1 , num2})`);
+}else{
+    if (num1 == num3) {
+        console.log(`El primer y tercer numero son iguales (${num1 , num3})`);
+    }else if (num2 == num3) {
+        console.log(`El segundo y tercer numero son iguales (${num2 , num3})`);
+    }else{
+        console.log('Los tres numeros son diferentes');
+        
+    }
+}
+
+
+git
